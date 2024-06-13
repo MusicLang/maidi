d23328110a5f3eae05b3de5a16cf835eee3a4b24
GARDIN Florian <you@example.com>
2024-06-13 17:52:03 +0000

    Bump version: 0.12.14 → 0.12.15

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ requirements = (this_directory / "requirements.txt").read_text().splitlines()
 
 setup(
     name='maidi',
-    version='0.12.14',
+    version='0.12.15',
     author="Florian GARDIN",
     author_email="fgardin.pro@gmail.com",
     description="A python package for symbolic AI music inference",
