7abe603f533645cdf9bbaf7778eee157d0734a40
floriangardin <fgardin.pro@gmail.com>
2024-06-13 18:31:11 +0200

    change CI workflow

--- docs/conftest.py
+++ docs/conftest.py
@@ -1,4 +1,9 @@
 import doctest
 from doctest_setup import setup_doctest
+import  os
+import sys
+
+# Set Fake API_KEY
+os.environ['API_KEY'] = 'FAKE_API'
 setup_doctest(doctest)
 # -----------------------------------------------------------------------------
